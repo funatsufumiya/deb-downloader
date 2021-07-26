920d7f6c3b27ae1406832a8086c526d42efd76f5
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2021-07-26 09:41:46 +0900

    fix: changed argv order

--- deb-dl-url.rb
+++ deb-dl-url.rb
@@ -4,15 +4,23 @@ require 'nokogiri'
 require 'open-uri'
 
 s = ARGV[0]
-v = ARGV[1] || "stretch"
-arch = ARGV[2] || "armhf"
+arch = ARGV[1] || "armhf"
+v = ARGV[2] || "stretch"
 base_url = 'https://packages.debian.org'
 url = "#{base_url}/#{v}/#{s}"
 doc = Nokogiri::HTML(URI.open(url))
-doc.css("#pdownload th a").each do |e|
-    sn = e.content
-    if sn == arch
+if arch == "_"
+    doc.css("#pdownload th a").each do |e|
+        sn = e.content
         link = e['href']
         puts "#{base_url}#{link}"
+    end
+else
+    doc.css("#pdownload th a").each do |e|
+        sn = e.content
+        if sn == arch
+            link = e['href']
+            puts "#{base_url}#{link}"
+        end
     end
 end
