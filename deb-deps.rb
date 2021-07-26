3cc209802ec5bce0899e17a8c2a3d8aa0ef85fc0
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2021-07-26 15:01:51 +0900

    feat: Add deb-deps.rb

---
+++ deb-deps.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+if ARGV.length == 0 || ARGV[0] == "-h" || ARGV[0] == "--help"
+    puts "usage: deb-deps.rb libglib2.0-0 [stretch]"
+    exit
+end
+
+s = ARGV[0]
+v = ARGV[1] || "stretch"
+base_url = 'https://packages.debian.org'
+url = "#{base_url}/#{v}/#{s}"
+doc = Nokogiri::HTML(URI.open(url))
+
+list = doc.css("#pdeps span.nonvisual + a").map do |e|
+    ed = e.parent.css("span.nonvisual").last
+    if !ed.nil? && ed.text == "dep:"
+        e.content
+    else
+        nil
+    end
+end
+
+list.reject!{ |x| x.nil? }.uniq!
+
+list.each do |e|
+    puts e
+end
